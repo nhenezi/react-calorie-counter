f6e34dc3efc126f5cbffaeaeb84ca03d5dd95756
Nikola Henezi <nikola@henezi.com>
2015-04-24 15:02:12 +0200

    Adding /auth endpoint (POST and GET)

---
+++ backend/http.py
@@ -0,0 +1,95 @@
+import bcrypt
+import cherrypy
+import models
+
+cherrypy.config.update('sever.confg')
+
+import json
+import datetime
+class DateEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if hasattr(obj, 'isoformat'):
+            return obj.isoformat()
+        else:
+            return str(obj)
+        return json.JSONEncoder.default(self, obj)
+
+
+json_encoder = DateEncoder()
+
+def json_handler(*args, **kwargs):
+    # Adapted from cherrypy/lib/jsontools.py
+    value = cherrypy.serving.request._json_inner_handler(*args, **kwargs)
+    return json_encoder.iterencode(value)
+
+class Auth(object):
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out(handler=json_handler)
+    def GET(self, access_token):
+        """
+        Retrieves authentication status.
+
+        If `access_token` is valid it will return user information.
+        """
+        session = models.Session()
+        user = models.User.get_from_token(access_token, session)
+        resp = {}
+        if user:
+            resp = user.to_json()
+        else:
+            resp = {'success': False}
+        session.close()
+        return resp
+
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out(handler=json_handler)
+    def POST(self):
+        """
+        Tries to login user with email/password combination.
+
+        If login is successful new access_token is generated
+        """
+        data = cherrypy.request.json
+        if 'password' not in data or 'email' not in data:
+            return {'success': False, 'error': 'Invalid username or password'}
+
+        email = data['email'].lower()
+        password = data['password']
+
+        session = models.Session()
+        user = session.query(models.User).filter_by(email=email).first()
+        if not user:
+            session.close()
+            return {'success': False, 'error': 'Invalid username or password'}
+
+        if not bcrypt.hashpw(password, user.password) == user.password:
+            return {'success': False, 'error': 'Invalid username or password'}
+
+        user.generate_new_token()
+        resp = user.to_json()
+        session.close()
+
+        return {'success': True, 'user': resp}
+
+
+class Meal(object):
+    pass
+
+class User(object):
+    pass
+
+class Root(object):
+    def __init__(self):
+        self.auth = Auth()
+        self.meal = Meal()
+        self.user = User()
+
+if __name__ == '__main__':
+    conf = {
+        '/': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher()
+        }
+    }
+
+    cherrypy.quickstart(Root(), '/', conf)
+
